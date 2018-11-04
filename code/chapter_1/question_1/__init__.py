730c09c5dc523b368219b0457641afc6b1de59e8
phamhoangtuan <phamhoangtuan.it@gmail.com>
2018-11-05 00:39:32 +0800

    Implement algorithm

--- code/chapter_1/question_1/__init__.py
+++ code/chapter_1/question_1/__init__.py
@@ -1,2 +1,10 @@
 def is_unique(input_string):
+	if input_string:
+		temp_dict = {}
+		for char in input_string:
+			if temp_dict.get(char):
+				return False
+			else:
+				temp_dict[char] = 1
+		return True
 	return False
