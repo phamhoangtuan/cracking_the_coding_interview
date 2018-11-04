89cd6562a4d65b880279cd80af8c68f9e2dd91b5
phamhoangtuan <phamhoangtuan.it@gmail.com>
2018-11-04 22:36:46 +0800

    Add test cases

--- test/chapter_1/question_1/test_solution.py
+++ test/chapter_1/question_1/test_solution.py
@@ -3,4 +3,19 @@ from code.chapter_1.question_1 import is_unique
 
 def test_none_string():
 	result = is_unique(None)
-	assert result is None
+	assert result is False
+
+
+def test_empty_string():
+	result = is_unique('')
+	assert result is False
+
+
+def test_no_unique_characters():
+	result = is_unique('abcdefabc')
+	assert result is False
+
+
+def test_unique_characters():
+	result = is_unique('abcdef')
+	assert result is True
