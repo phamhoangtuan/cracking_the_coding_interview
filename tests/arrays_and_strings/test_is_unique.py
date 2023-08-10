de6befd0e87d19efff9b56c9965117433c9042b6
Tuan Pham <phamhoangtuan.it@gmail.com>
2023-08-10 23:40:50 +0700

    feat: add test

--- tests/arrays_and_strings/test_is_unique.py
+++ tests/arrays_and_strings/test_is_unique.py
@@ -16,3 +16,4 @@ def test_is_unique_no_additional_data_structure() -> None:
     assert is_unique_no_additional_data_structure('aabbcc') is False
     assert is_unique_no_additional_data_structure('') is True
     assert is_unique_no_additional_data_structure(' ') is True
+    assert is_unique_no_additional_data_structure('a') is True
